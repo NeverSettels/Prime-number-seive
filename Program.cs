5cae0e20bec5e06f23baa51b0e6b7080608e1fdf
Eric Settels and Joseph Wangemann <eric+joseph@>
2020-02-26 14:05:48 -0800

    add method for sieving prime numbers from list

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 using System;
 using System.Collections.Generic;
+using Prime.Models;
 
 namespace Prime
 {
   public class Program
   {
-    static void Main()
+   public static void Main()
     {
-      
+      Console.WriteLine("****************************************");
+      Console.WriteLine("********PRIME NUMBER FINDER*************");
+      Console.Write("Please enter a whole number: ");
+      int number = int.Parse(Console.ReadLine());
+      Numbers.PopulateList(number);
+      Numbers.SieveList();
     }
   }
-
 }
--- models/Prime.cs
+++ models/Prime.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Prime.Models
@@ -16,9 +17,20 @@ namespace Prime.Models
 
     public static void SieveList()
     {
+      int counter = numbersList.Count;
+      for (int j = 0; j<numbersList.Count; j++)
+      {
+        for(int i = numbersList[j]; i<=counter+1; i++)
+        {
+          if(i/numbersList[j] != 1 && i%numbersList[j] == 0)
+          {
+            numbersList.Remove(i);
+          }
+        }
+      }
       foreach (int number in numbersList)
       {
-        if ()
+      Console.WriteLine(number);
       }
     }
   }
